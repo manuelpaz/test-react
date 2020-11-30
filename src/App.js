1410749081a970823bc646dec138f81b00ebf0a8
manuelpaz <manuelpazrobles@gmail.com>
2020-11-29 20:07:40 -0600

    modificando para ver el cambio en la pagina

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Estoy editando la como se sugiere
         </p>
         <a
           className="App-link"
