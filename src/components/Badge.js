3b166567e8a3a896013b8a18724277aceb536264
manuelpaz <manuelpazrobles@gmail.com>
2020-11-30 13:10:01 -0600

    Se crea el componente Badge con mas complejidad

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
+import confLogo from '../images/badge-header.svg'
 class Badge extends React.Component{
     //Render define el resultado que vamo a ver en pantalla
     render(){
         return <div>
             <div>
-                <img src="" alt="Logo de la conferencia"/>
+                <img src={confLogo} alt="Logo de la conferencia"/>
             </div>
             <div>
-                <img src = "" alt="Avatar"/>
+                <img src = "https://s.gravatar.com/avatar/49c401b8c20d11caa2246b058cef0467?s=80" alt="Avatar"/>
                 <h1>Manuel<br/>Paz</h1>
             </div>
             <div>
