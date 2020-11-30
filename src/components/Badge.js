9c57b580d113e8d462137e5b14368b310c40ef7d
manuelpaz <manuelpazrobles@gmail.com>
2020-11-30 17:30:36 -0600

    Se defininen los props y  permite reusar el compon

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -4,18 +4,18 @@ import "./styles/Badge.css";
 import confLogo from '../images/badge-header.svg';
 class Badge extends React.Component{
     //Render define el resultado que vamo a ver en pantalla
-    render(){
+    render(){        
         return <div className="Badge">
             <div className="Badge__header">
                 <img src={confLogo} alt="Logo de la conferencia"/>
             </div>
             <div className="Badge__section-name">
-                <img className="Badge__avatar" src = "https://s.gravatar.com/avatar/49c401b8c20d11caa2246b058cef0467?s=80" alt="Avatar"/>
-                <h1>Manuel<br/>Paz</h1>
+                <img className="Badge__avatar" src = {this.props.urlAvatar} alt="Avatar"/>
+                <h1>{this.props.firstName}<br/>{this.props.lastName}</h1>
             </div>
             <div className="Badge__section-info">
-                <h3>Estudiante Frontend</h3>
-                <div>@manuelpazrobles</div>
+                <h3>{this.props.jobTitle}</h3>
+                <div>@{this.props.twitter}</div>
             </div>
             <div className="Badge__footer">
                 #platziconf
--- src/index.js
+++ src/index.js
@@ -7,4 +7,10 @@ import Badge from  './components/Badge';
 
 const container = document.getElementById('app');
  //ReacDom.render(__qué__, __dónde__)
-ReacDom.render(<Badge />, container);
+ReacDom.render(
+<Badge firstName="Manuel"
+ lastName ="Paz" 
+ jobTitle="Estudiante machine Learning" 
+ twitter="manuelpazrobles"
+ urlAvatar = "https://s.gravatar.com/avatar/49c401b8c20d11caa2246b058cef0467?s=80" />,
+ container);
