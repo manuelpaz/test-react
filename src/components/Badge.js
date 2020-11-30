9a88cad976d2db322e83ca038afaba9e0415f3b7
manuelpaz <manuelpazrobles@gmail.com>
2020-11-30 12:11:09 -0600

    Se empiza con la creaciín del Badge

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -3,7 +3,22 @@ import React from 'react';
 class Badge extends React.Component{
     //Render define el resultado que vamo a ver en pantalla
     render(){
-        return <h1>Badge!!!</h1>;
+        return <div>
+            <div>
+                <img src="" alt="Logo de la conferencia"/>
+            </div>
+            <div>
+                <img src = "" alt="Avatar"/>
+                <h1>Manuel<br/>Paz</h1>
+            </div>
+            <div>
+                <p>Estudiante Frontend</p>
+                <p>@manuelpazrobles</p>
+            </div>
+            <div>
+                #platziconf
+            </div>
+        </div>;
     }
 }
 
