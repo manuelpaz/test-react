37ea3d9a87918d3f09cc71d921b9cabe82d636f3
manuelpaz <manuelpazrobles@gmail.com>
2020-11-30 12:03:40 -0600

    Se crea el primer componente

---
+++ src/components/Badge.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Badge extends React.Component{
+    //Render define el resultado que vamo a ver en pantalla
+    render(){
+        return <h1>Badge!!!</h1>;
+    }
+}
+
+export default Badge;
--- src/index.js
+++ src/index.js
@@ -1,44 +1,10 @@
-//const element = document.createElement('h1');
-//element.innerText = 'Hello, Platzi Badges!'; // se da un texto al elemento que se creo
+import React from  'react';
+import ReacDom from 'react-dom';
 
-//Donde se quiere poner el elemento en el elemento app de App.js
-//const container = document.getElementById('app');
+import Badge from  './components/Badge';
 
-//container.appendChild(element);
- //Se utilizara react para hacer lo mismo que lo anterior
 
- import React from  'react';
- import ReacDom from 'react-dom';
-
- //const jsx = <h1>Hello, Platzi Badges from React!</h1>;
- 
- //const element = React.createElement('a', {href: 'https://platzi.com'}, 'Ir a Platzi'); //Alternativa
- //const container = document.getElementById('app');
- const name = 'Manuel-Tony'
-/*const element = React.createElement(
-  'h1',
-  {},
-  `Hola soy ${name}`
-);*/
-const sum = ()=> 3 + 3;
-//Se utiliza jsx con función
-//const jsx = <h1>Hola soy, {sum()}</h1>;
-
-const jsx = (
-  <div>
-    <h1>Hola, soy {name}</h1>
-    <p>Soy estudiante frontend.</p>
-  </div>
-);
-
-//Crear lo anterior sin jsx es mas complicado
-const element = React.createElement(
-  'div',
-  {},
-  React.createElement('h1', {}, `Hola soy ${name}`),
-  React.createElement('p', {}, 'Soy estudiante de la Web!')
-);
 
 const container = document.getElementById('app');
  //ReacDom.render(__qué__, __dónde__)
-ReacDom.render(element, container);
+ReacDom.render(<Badge />, container);
