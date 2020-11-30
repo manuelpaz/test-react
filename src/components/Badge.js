0d2758fa6c5fd66d7165a1d78ff65091274e0e39
manuelpaz <manuelpazrobles@gmail.com>
2020-11-30 15:00:53 -0600

    Se agregan estilos

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,26 +1,26 @@
 import React from 'react';
 
-import confLogo from '../images/badge-header.svg'
+import "./styles/Badge.css";
+import confLogo from '../images/badge-header.svg';
 class Badge extends React.Component{
     //Render define el resultado que vamo a ver en pantalla
     render(){
-        return <div>
-            <div>
+        return <div className="Badge">
+            <div className="Badge__header">
                 <img src={confLogo} alt="Logo de la conferencia"/>
             </div>
-            <div>
-                <img src = "https://s.gravatar.com/avatar/49c401b8c20d11caa2246b058cef0467?s=80" alt="Avatar"/>
+            <div className="Badge__section-name">
+                <img className="Badge__avatar" src = "https://s.gravatar.com/avatar/49c401b8c20d11caa2246b058cef0467?s=80" alt="Avatar"/>
                 <h1>Manuel<br/>Paz</h1>
             </div>
-            <div>
-                <p>Estudiante Frontend</p>
-                <p>@manuelpazrobles</p>
+            <div className="Badge__section-info">
+                <h3>Estudiante Frontend</h3>
+                <div>@manuelpazrobles</div>
             </div>
-            <div>
+            <div className="Badge__footer">
                 #platziconf
             </div>
         </div>;
     }
 }
-
 export default Badge;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from  'react';
 import ReacDom from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 
+import './global.css';
 import Badge from  './components/Badge';
-
-
 
 const container = document.getElementById('app');
  //ReacDom.render(__qué__, __dónde__)
