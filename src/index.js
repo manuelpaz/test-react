b834f1eff624b29b8347690e6b2fdf81e10fe982
manuelpaz <manuelpazrobles@gmail.com>
2020-11-30 18:08:17 -0600

    Se crea una pagina y el navbar

--- src/index.js
+++ src/index.js
@@ -3,14 +3,9 @@ import ReacDom from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import './global.css';
-import Badge from  './components/Badge';
+import Badge from  './pages/BadgeNew';
+import BadgeNew from './pages/BadgeNew';
 
 const container = document.getElementById('app');
  //ReacDom.render(__qué__, __dónde__)
-ReacDom.render(
-<Badge firstName="Manuel"
- lastName ="Paz" 
- jobTitle="Estudiante machine Learning" 
- twitter="manuelpazrobles"
- urlAvatar = "https://s.gravatar.com/avatar/49c401b8c20d11caa2246b058cef0467?s=80" />,
- container);
+ReacDom.render(<BadgeNew />,container);
