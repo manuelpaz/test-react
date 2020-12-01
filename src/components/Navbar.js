f21337750912663a51d24fb8eccdbf3ee9b41bc0
manuelpaz <manuelpazrobles@gmail.com>
2020-11-30 18:17:56 -0600

    Se continua con el navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import logo from '../images/logo.svg'
 class Navbar extends React.Component{
     render(){
-        return(
-           <p>Navbar</p>
-        );       
+        return <div>
+            <img src={logo} alt="Logo"/>
+            <span className="font-weight-light">Platzi</span>
+            <span className="font-weight-bold">Conf</span>
+        </div>;     
     }
 }
 
