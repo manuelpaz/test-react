8539b2cdd7fda4d58a2007719644317b0314c263
manuelpaz <manuelpazrobles@gmail.com>
2020-11-30 20:09:25 -0600

    Se da continuidad a navnar se agrega badge

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
-import logo from '../images/logo.svg'
+import './styles/Navbar.css';
+import logo from '../images/logo.svg';
 class Navbar extends React.Component{
     render(){
-        return <div>
-            <img src={logo} alt="Logo"/>
-            <span className="font-weight-light">Platzi</span>
-            <span className="font-weight-bold">Conf</span>
+        return <div className="Navbar">
+            <div className="container-fluid">
+            <a className="Navbar__brand" href="/">
+                <img className = "Navbar__logo" src={logo} alt="Logo"/>
+                <span className="font-weight-light">Platzi</span>
+                <span className="font-weight-bold">Conf</span>
+            </a>   
+            </div>         
         </div>;     
     }
 }
--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,13 +1,32 @@
 import React from 'react';
 
+import './styles/BadgeNew.css'
+import header from '../images/badge-header.svg'
 import Navbar from '../components/Navbar';
+import Badge from '../components/Badge';
 
 class BadgeNew extends React.Component{
     render(){
         return(
             <div>
                 <Navbar />
-            </div>
+                <div className="BadgeNew__hero">
+                    <img className="img-fluid" src={header} alt="Logo" />
+                </div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <Badge 
+                                firstName="Manuel"
+                                lastName="Paz"
+                                twitter="manuelpazrobles"
+                                jobTitle="Alumno Platzi"
+                                urlAvatar="https://s.gravatar.com/avatar/49c401b8c20d11caa2246b058cef0467?s=80"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>            
         );       
     }
 }
