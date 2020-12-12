b510ea61eaa4141651181b14ffe7dcd79de6cdea
manuelpaz <manuelpazrobles@gmail.com>
2020-12-11 21:52:27 -0600

    Practica ciclo de vida componentes.      Se introducen los metodos que se suceden durante el ciclo de vida      de un componente. Se utiliza una funcion setTimeout para simular      una petición asincrona en este commit se observa un error al      terminar un componente cuando aun no se completo la petición      asincrona lo cual se corregira en el siguiente commit.

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -7,40 +7,77 @@ import BadgesList from '../components/BadgesList';
 
 
 
-class Badges extends React.Component{
-    state = {
-        data:[
-            {
-              id: "2de30c42-9deb-40fc-a41f-05e62b5939a7",
-              firstName: "Bobby",
-              lastName: "Fischer",
-              email: "bobbyfischer@gmail.com",
-              jobTitle: "World Champion 1972-1975",
-              twitter: "bobby_fischer", 
-              avatarUrl:   require("../images/Bobby-Fischer.jpg").default        
-            },
-            {
-              id: "d00d3614-101a-44ca-b6c2-0be075aeed3d",
-              firstName: "Raul",
-              lastName: "Capablanca",
-              email: "capa_blanca@hotmail.com",
-              jobTitle: "World champion 1921-1927",
-              twitter: "capablanca",
-              avatarUrl:  require("../images/Raul-Capablanca.jpg").default               
-            },
-            {
-              id: "63c03386-33a2-4512-9ac1-354ad7bec5e9",
-              firstName: "Garry",
-              lastName: "Kasparov",
-              email: "garry_kasparov@hotmail.com",
-              jobTitle: "World champion 1985-2000",
-              twitter: "kasparov", 
-              avatarUrl:   require("../images/Garry-Kasparov.jpg").default              
-            }
-          ],
-    };
+class Badges extends React.Component{   
+
+    constructor(props){
+        super(props); 
+        console.log('1. constructor()');
+        this.state = { 
+            data:[],
+        };
+    }
+
+    componentDidMount(){
+        console.log('3. componentDidMount()');
+        //Se simula una petición asincrona.
+        setTimeout(() =>{
+            this.setState({
+                data:[
+                    {
+                      id: "2de30c42-9deb-40fc-a41f-05e62b5939a7",
+                      firstName: "Bobby",
+                      lastName: "Fischer",
+                      email: "bobbyfischer@gmail.com",
+                      jobTitle: "World Champion 1972-1975",
+                      twitter: "bobby_fischer", 
+                      avatarUrl:   require("../images/Bobby-Fischer.jpg").default        
+                    },
+                    {
+                      id: "d00d3614-101a-44ca-b6c2-0be075aeed3d",
+                      firstName: "Raul",
+                      lastName: "Capablanca",
+                      email: "capa_blanca@hotmail.com",
+                      jobTitle: "World champion 1921-1927",
+                      twitter: "capablanca",
+                      avatarUrl:  require("../images/Raul-Capablanca.jpg").default               
+                    },
+                    {
+                      id: "63c03386-33a2-4512-9ac1-354ad7bec5e9",
+                      firstName: "Garry",
+                      lastName: "Kasparov",
+                      email: "garry_kasparov@hotmail.com",
+                      jobTitle: "World champion 1985-2000",
+                      twitter: "kasparov", 
+                      avatarUrl:   require("../images/Garry-Kasparov.jpg").default              
+                    }
+                  ],
+            })
+        }, 3000)
+    }
+
+    //Antes de que se realice una actualización ocurre el siguiente metodo
+    //El cual recibe dos argumentos los cuales son los props que se tenian antes
+    //y el state que se tenia antes
+    componentDidUpdate(prevProps, prevState){
+        console.log('5. componentDidUpdate()');
+        //Se imprime en consola los props y el state previos.
+        console.log({
+            prevProps: prevProps, prevState: prevState
+        });
+
+        console.log({
+            props: this.props, 
+            state: this.state,
+        })
+    }
+
+    //El siguiente metodo sucede antes de que se vaya el componente del DOM
+    componentWillUnmount(){
+        console.log('6. componentWillUnmount()'); 
+    }
 
     render(){
+        console.log('2/4. render()');
        return(
             <div className="Listado">                
                 <div className="Badges">
