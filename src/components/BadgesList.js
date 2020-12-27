b06d6ee7ebaf97e6437f929663303f5a59f1c3c0
manuelpaz <manuelpazrobles@gmail.com>
2020-12-26 21:03:02 -0600

    Caso de no haber datos.   En este commit se simula la ausencia de datos a la petición.   En el siguiente commit se simulara la presencia de un error y el   manejo del mismo.

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import BadgeListItem from '../components/BadgeListItem';
+import {Link} from 'react-router-dom';
 import './styles/BadgesList.css';
 class BadgesList extends React.Component {
     render() {
+      if(this.props.Listbadges.length === 0){
+        return(
+          <div>
+            <h3>No badges were found</h3>
+            <Link className="btn btn-primary" to="/badges/new">
+              Create new badge
+            </Link>
+          </div>
+        )
+      }
       return (
         <div className="BadgesList">
           <ul className="list-unstyled">
