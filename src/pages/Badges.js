445bf1c25852c466bb2bb70d5e0698a347dbd6fd
manuelpaz <manuelpazrobles@gmail.com>
2020-12-26 19:34:07 -0600

    Peticiones GET        En este commit se hace el uso de peticiones GET para lo cual        se agrega al proyecto los archivos listados y las modificaciones        presentadas a la pagina Badges.        Tambien se realizo la instalación de nuevas dependencias        npm i faker -D        npm i json-server -D        npm i npm-run-all -D        En el proximo commit se mostrara        el manejo de un error en caso de haberlo.

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -4,82 +4,32 @@ import './styles/Badges.css';
 import confLogo from   '../images/badge-header.svg';
 import BadgesList from '../components/BadgesList';
 
+import api from '../api'
 
+class Badges extends React.Component{     
+    state ={
+        loading: true,
+        error: null,
+        data: undefined,       
 
-
-class Badges extends React.Component{   
-
-    constructor(props){
-        super(props); 
-        console.log('1. constructor()');
-        this.state = { 
-            data:[],
-        };
-    }
-
+    };
     componentDidMount(){
-        console.log('3. componentDidMount()');
-        //Se simula una petición asincrona.
-        this.timeoutId =  setTimeout(() =>{
-            this.setState({
-                data:[
-                    {
-                      id: "2de30c42-9deb-40fc-a41f-05e62b5939a7",
-                      firstName: "Bobby",
-                      lastName: "Fischer",
-                      email: "bobbyfischer@gmail.com",
-                      jobTitle: "World Champion 1972-1975",
-                      twitter: "bobby_fischer", 
-                      avatarUrl:   require("../images/Bobby-Fischer.jpg").default        
-                    },
-                    {
-                      id: "d00d3614-101a-44ca-b6c2-0be075aeed3d",
-                      firstName: "Raul",
-                      lastName: "Capablanca",
-                      email: "capa_blanca@hotmail.com",
-                      jobTitle: "World champion 1921-1927",
-                      twitter: "capablanca",
-                      avatarUrl:  require("../images/Raul-Capablanca.jpg").default               
-                    },
-                    {
-                      id: "63c03386-33a2-4512-9ac1-354ad7bec5e9",
-                      firstName: "Garry",
-                      lastName: "Kasparov",
-                      email: "garry_kasparov@hotmail.com",
-                      jobTitle: "World champion 1985-2000",
-                      twitter: "kasparov", 
-                      avatarUrl:   require("../images/Garry-Kasparov.jpg").default              
-                    }
-                  ],
-            })
-        }, 3000);
-
+        this.fetchData()
     }
+    fetchData = async () =>{
+        this.setState({loading: true, error: null})
 
-    //Antes de que se realice una actualización ocurre el siguiente metodo
-    //El cual recibe dos argumentos los cuales son los props que se tenian antes
-    //y el state que se tenia antes
-    componentDidUpdate(prevProps, prevState){
-        console.log('5. componentDidUpdate()');
-        //Se imprime en consola los props y el state previos.
-        console.log({
-            prevProps: prevProps, prevState: prevState
-        });
-
-        console.log({
-            props: this.props, 
-            state: this.state,
-        })
-    }
-
-    //El siguiente metodo sucede antes de que se vaya el componente del DOM
-    componentWillUnmount(){
-        console.log('6. componentWillUnmount()'); 
-        clearTimeout(this.timeoutId);
-    }
-
-    render(){
-        console.log('2/4. render()');
+        try{
+            const data = await api.badges.list();
+            this.setState({loading: false, data: data})
+        }catch(error){
+            this.setState({loading: false, error: error})
+        }
+    };
+    render(){  
+        if(this.state.loading===true){
+            return 'Loading...';
+        }
        return(
             <div className="Listado">                
                 <div className="Badges">
