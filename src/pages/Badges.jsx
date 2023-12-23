e36a12dfad8b5c71033a0cf99f3eb94771ca4381
Manuel Paz Robles <manuelpazrobles@gmail.com>
2023-12-22 22:45:03 -0600

    Implementacion del borrado de badge Se hacen las modificacione para el borrado de badges se siguio el codigo en el proyecto https://github.com/Alan2Luna/Platzi_badge/blob/master/src/pages/Badges.js

--- src/pages/Badges.jsx
+++ src/pages/Badges.jsx
@@ -1,54 +1,65 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import './styles/Badges.css';
-import confLogo from   '../images/badge-header.svg';
+import { Link } from 'react-router-dom';
+
+import './styles/Badges.css'
+import confLogo from '../images/badge-header.svg';
 import BadgesList from '../components/BadgesList';
 import PageLoading from '../components/PageLoading';
 import PageError from '../components/PageError';
+import MiniLoader from '../components/MiniLoader';
+import api from '../api';
 
-import api from '../api'
-
-class Badges extends React.Component{     
-    state ={
+class Badges extends React.Component {
+    state = {
         loading: true,
         error: null,
-        data: undefined,       
-
+        data: undefined
     };
-    componentDidMount(){
-        this.fetchData()
-    }
-    fetchData = async () =>{
-        this.setState({loading: true, error: null})
 
-        try{
+    componentDidMount() {
+        this.fetchData();
+
+        this.intervalId = setInterval(this.fetchData, 5000);
+    };
+
+    componentWillUnmount() {
+        clearInterval(this.intervalId);
+    }
+
+    fetchData = async () => {
+        this.setState({ loading: true, error: null});
+
+        try {
             const data = await api.badges.list();
-            this.setState({loading: false, data: data})
-        }catch(error){
-            this.setState({loading: false, error: error})
+            this.setState({ loading: false, data: data });
+        } catch (error) {
+            this.setState({ loading: false, error: error });
         }
     };
-    render(){  
-        if(this.state.loading===true){
+    
+    render() {
+        if (this.state.loading === true && !this.state.data) {
             return <PageLoading />;
         }
-        if(this.state.error){
-            //return `Error: ${this.state.error.message}`;
-            return <PageError error= {this.state.error}  />
+
+        if (this.state.error) {
+            return <PageError error={this.state.error}/>;
         }
-       return(
-            <div className="Listado">                
+
+        return (
+            <div className="Listado">
                 <div className="Badges">
                     <div className="Badges__hero">
                         <div className="Badges__container">
                             <img 
-                            src={confLogo} 
-                            alt="Conf Logo"
-                            className="Badges_conf-logo"
+                                src={confLogo} 
+                                className="Badges__conf-logo" 
+                                alt="Conf logo"
                             />
                         </div>
-                    </div>                    
+                    </div>
                 </div>
+
                 <div className="Badges__container" id="Badges_lista">
                     <div className="Badges__buttons">
                         <Link to="/badges/new" className="btn btn-primary">
@@ -60,9 +71,10 @@ class Badges extends React.Component{
                             <BadgesList  Listbadges={this.state.data} />                            
                         </div>
                     </div>
+                    {this.state.loading && <MiniLoader /> }
                 </div>
-            </div> 
-       );
+            </div>
+        )
     }
 }
 
